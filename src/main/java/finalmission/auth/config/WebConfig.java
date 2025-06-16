912c52181b6fa5b1cd7c23e8e6c34e820e82529b
7i.w__k <tizmfns1218@Naver.com>
2025-06-17 00:25:06 +0900

    feat: JWT 토큰 처리 기능 추가

--- src/main/java/finalmission/auth/config/WebConfig.java
+++ src/main/java/finalmission/auth/config/WebConfig.java
@@ -2,22 +2,20 @@ package finalmission.auth.config;
 
 import finalmission.auth.resolver.LoginMemberArgumentResolver;
 import java.util.List;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
-        resolvers.add(loginMemberArgumentResolver());
-    }
-
-    @Bean
-    public LoginMemberArgumentResolver loginMemberArgumentResolver() {
-        return new LoginMemberArgumentResolver();
+        resolvers.add(loginMemberArgumentResolver);
     }
 }
